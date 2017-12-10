b99c6cbf3cfc54546e9f1f6780b79c10c40213ce
Rover <rover.huang@gmail.com>
2017-12-10 13:43:38 +0800

    commit

---
+++ wordlist.js
@@ -0,0 +1,222 @@
+var wordlist = [
+  {
+    "hanzi": "導遊",
+    "pinyin": "dǎo yóu"
+  },
+  {
+    "hanzi": "一覽無遺",
+    "pinyin": "yì lǎn wú yí"
+  },
+  {
+    "hanzi": "天壇大佛",
+    "pinyin": "Tiāntán Dàfó"
+  },
+  {
+    "hanzi": "迪士尼樂園",
+    "pinyin": "Díshìní Lèyuán"
+  },
+  {
+    "hanzi": "挪亞方舟",
+    "pinyin": "Nuóyà Fāngzhōu"
+  },
+  {
+    "hanzi": "昂坪市集",
+    "pinyin": "Ángpíng Shìjí"
+  },
+  {
+    "hanzi": "打折",
+    "pinyin": "dǎ zhé"
+  },
+  {
+    "hanzi": "廟街",
+    "pinyin": "Miào Jiē"
+  },
+  {
+    "hanzi": "翠綠",
+    "pinyin": "cuì lǜ"
+  },
+  {
+    "hanzi": "鳥語花香",
+    "pinyin": "niǎo yǔ huā xiāng"
+  },
+  {
+    "hanzi": "涼亭",
+    "pinyin": "liáng tíng"
+  },
+  {
+    "hanzi": "暈倒",
+    "pinyin": "yūn dǎo"
+  },
+  {
+    "hanzi": "求救",
+    "pinyin": "qiú jiù"
+  },
+  {
+    "hanzi": "不省人事",
+    "pinyin": "bù shěng rén shì"
+  },
+  {
+    "hanzi": "傷者",
+    "pinyin": "shāng zhě"
+  },
+  {
+    "hanzi": "燒烤場",
+    "pinyin": "shāo kǎo chǎng"
+  },
+  {
+    "hanzi": "救護人員",
+    "pinyin": "jiù hù rén yuán"
+  },
+  {
+    "hanzi": "掏腰包",
+    "pinyin": "tāo yāo bāo"
+  },
+  {
+    "hanzi": "套餐",
+    "pinyin": "tào cān"
+  },
+  {
+    "hanzi": "牛肉串兒",
+    "pinyin": "niú ròu chuànr"
+  },
+  {
+    "hanzi": "椒鹽九肚魚",
+    "pinyin": "jiāo yán jiǔ dù yú"
+  },
+  {
+    "hanzi": "日式豆腐鍋",
+    "pinyin": "Rì shì dòu fu guō"
+  },
+  {
+    "hanzi": "微波爐",
+    "pinyin": "wēi bō lú"
+  },
+  {
+    "hanzi": "結帳",
+    "pinyin": "jié zhàng"
+  },
+  {
+    "hanzi": "優惠",
+    "pinyin": "yōu huì"
+  },
+  {
+    "hanzi": "刷卡",
+    "pinyin": "shuā kǎ"
+  },
+  {
+    "hanzi": "獨居長者",
+    "pinyin": "dú jū zhǎng zhě"
+  },
+  {
+    "hanzi": "證件",
+    "pinyin": "zhèng jiàn"
+  },
+  {
+    "hanzi": "義工",
+    "pinyin": "yì gōng"
+  },
+  {
+    "hanzi": "織",
+    "pinyin": "zhī"
+  },
+  {
+    "hanzi": "搞衞生",
+    "pinyin": "gǎo wèi shēng"
+  },
+  {
+    "hanzi": "牀鋪",
+    "pinyin": "chuáng pū"
+  },
+  {
+    "hanzi": "悶悶不樂",
+    "pinyin": "mèn mèn bù lè"
+  },
+  {
+    "hanzi": "膽小怕事",
+    "pinyin": "dǎn xiǎo pà shì"
+  },
+  {
+    "hanzi": "羨慕",
+    "pinyin": "xiàn mù"
+  },
+  {
+    "hanzi": "樂觀",
+    "pinyin": "lè guān"
+  },
+  {
+    "hanzi": "感染",
+    "pinyin": "gǎn rǎn"
+  },
+  {
+    "hanzi": "開朗",
+    "pinyin": "kāi lǎng"
+  },
+  {
+    "hanzi": "速算",
+    "pinyin": "sù suàn"
+  },
+  {
+    "hanzi": "過獎",
+    "pinyin": "guò jiǎng"
+  },
+  {
+    "hanzi": "咖喱",
+    "pinyin": "kā lí"
+  },
+  {
+    "hanzi": "沙麗",
+    "pinyin": "shā lì"
+  },
+  {
+    "hanzi": "鬧彆扭",
+    "pinyin": "nào biè niu"
+  },
+  {
+    "hanzi": "領情",
+    "pinyin": "lǐng qíng"
+  },
+  {
+    "hanzi": "印度教",
+    "pinyin": "Yìndù jiào"
+  },
+  {
+    "hanzi": "教徒",
+    "pinyin": "jiào tú"
+  },
+  {
+    "hanzi": "蠢",
+    "pinyin": "chǔn"
+  },
+  {
+    "hanzi": "文化傳統",
+    "pinyin": "wén huà chuán tǒng"
+  },
+  {
+    "hanzi": "寬宏大量",
+    "pinyin": "kuān hóng dà liàng"
+  },
+  {
+    "hanzi": "握手言和",
+    "pinyin": "wò shǒu yán hé"
+  },
+  {
+    "hanzi": "不自量力",
+    "pinyin": "bú zì liàng lì"
+  },
+  {
+    "hanzi": "琴譜",
+    "pinyin": "qín pǔ"
+  },
+  {
+    "hanzi": "呆呆地",
+    "pinyin": "dāi dāi de"
+  },
+  {
+    "hanzi": "硬着頭皮",
+    "pinyin": "yìng zhe tóu pí"
+  },
+  {
+    "hanzi": "馬大哈",
+    "pinyin": "mǎ dà hā"
+  }
+]
